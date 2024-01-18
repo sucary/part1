544c24be619f3abc7948fb2a9fd0298a2bc585d0
Haocheng Su <haocsu@utu.fi>
2024-01-18 22:06:07 +0200

    Complete 1.3

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,25 @@
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
       <Header course = {course} />
-      <p>
-        <Content part1 = {part1} exercises1 = {exercises1} />
-        <Content part2 = {part2} exercises2 = {exercises2} />
-        <Content part3 = {part3} exercises3 = {exercises3} />
-      </p>
-      <p><Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} /></p>
+        <Content part1 = {part1}
+        part2 = {part2}
+        part3 = {part3} />
+      <Total exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises} />
     </div>
   )
 }
@@ -32,7 +36,7 @@ const Header = (props) => {
 const Part = (props) => {
   return (
     <div>
-      {props.part} {props.exercises}
+      <p>{props.part} {props.exercises}</p>
     </div>
   )
 
@@ -41,11 +45,11 @@ const Part = (props) => {
 const Content = (props) => {
   return(
     // To render the "Part" component
-    // Data to be passed to the "Part" component: part, exercises
+    // Data to be passed to the "Part" component: part, name/exercises
     <div>
-      <Part part = {props.part1} exercises = {props.exercises1} />
-      <Part part = {props.part2} exercises = {props.exercises2} />
-      <Part part = {props.part3} exercises = {props.exercises3} />
+      <Part part = {props.part1.name} exercises = {props.part1.exercises} />
+      <Part part = {props.part2.name} exercises = {props.part2.exercises} />
+      <Part part = {props.part3.name} exercises = {props.part3.exercises} />
     </div>
   )
 }
