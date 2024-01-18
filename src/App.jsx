c276992c76f3678a2da2f8298608152d160ae87b
Haocheng Su <haocsu@utu.fi>
2024-01-18 22:29:28 +0200

    Complete 1.4

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,28 @@
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    // The table has 3 elements (objects), each has 2 properties.
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header course = {course} />
-        <Content part1 = {part1}
-        part2 = {part2}
-        part3 = {part3} />
-      <Total exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises} />
+        <Content part1 = {parts[0]}
+        part2 = {parts[1]}
+        part3 = {parts[2]} />
+      <Total exercises1 = {parts[0].exercises} exercises2 = {parts[1].exercises} exercises3 = {parts[2].exercises} />
     </div>
   )
 }
@@ -45,7 +48,8 @@ const Part = (props) => {
 const Content = (props) => {
   return(
     // To render the "Part" component
-    // Data to be passed to the "Part" component: part, name/exercises
+    // Data to be passed to the "Part" component: {object.property}
+    // The objects are defined in return block of APP
     <div>
       <Part part = {props.part1.name} exercises = {props.part1.exercises} />
       <Part part = {props.part2.name} exercises = {props.part2.exercises} />
