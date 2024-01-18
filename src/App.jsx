02be660dd72a9fe4b1f77a923bbe286494df5495
Haocheng Su <haocsu@utu.fi>
2024-01-18 22:48:20 +0200

    Complete 1.5

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,29 @@
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    // The table has 3 elements (objects), each has 2 properties.
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course = {course} />
-        <Content part1 = {parts[0]}
-        part2 = {parts[1]}
-        part3 = {parts[2]} />
-      <Total exercises1 = {parts[0].exercises} exercises2 = {parts[1].exercises} exercises3 = {parts[2].exercises} />
+      <Header course = {course.name} />
+        <Content part1 = {course.parts[0]}
+        part2 = {course.parts[1]}
+        part3 = {course.parts[2]} />
+      <Total exercises1 = {course.parts[0].exercises} exercises2 = {course.parts[1].exercises} exercises3 = {course.parts[2].exercises} />
     </div>
   )
 }
